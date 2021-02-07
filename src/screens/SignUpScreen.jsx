c7ac1a327d8eed0874ab3547b67a6fb7647e8a8f
issy <77483468+Issy-R@users.noreply.github.com>
2021-02-07 18:28:35 +0900

    エラ〜メッセージの翻訳

--- src/screens/SignUpScreen.jsx
+++ src/screens/SignUpScreen.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { View, Text, TextInput, StyleSheet, TouchableOpacity, Alert, } from 'react-native';
 import firebase from 'firebase';
-
+import { translateErrors } from '../utils';
 import Button from '../components/Button';
 
 
@@ -23,7 +23,8 @@ export default function SignUpScreen(props) {
       })
       .catch((error) => {
         console.log(error.code, error.message);
-        Alert.alert(error.code);
+        const errorMsg = translateErrors(error.code);
+        Alert.alert(errorMsg.title, errorMsg.description);
       });
   }
 
